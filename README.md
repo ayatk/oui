62d215616dab3bddce03a415a692b1a1bc42af30
Aya Tokikaze <git@ayatk.com>
2016-05-02 11:15:03 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+oui
+====
+search vender information from OUI(Organizationally Unique Identifier)
+
+## Usage
+
+    $ oui <MAC Address>
+
+e.g.
+
+    $ oui 000000 # 00-00-00 or 00:00:00
+    XEROX CORPORATION
+
+## License
+### OUI(Organizationally Unique Identifier) Data
+Copyright 2016 [IEEE][ieee] - All rights reserved.
+
+### Source Code
+Released under the [MIT License][license] /
+Copyright (c) 2016 Aya Tokikaze.
+
+[ieee]: http://www.ieee.org/
+[license]: https://github.com/ayatk/oui/blob/master/LICENSE
---
+++ main.go
@@ -0,0 +1,80 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2016 Aya Tokikaze
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/codegangsta/cli"
+)
+
+const (
+	cliName        = "oui"
+	cliDescription = "search vender information for OUI(Organizationally Unique Identifier)"
+	version        = "v0.1.0"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = cliName
+	app.Usage = cliDescription
+	app.Version = version
+	app.Flags = []cli.Flag{
+		cli.HelpFlag,
+	}
+	app.Action = func(c *cli.Context) {
+		if c.NArg() == 0 {
+			cli.ShowAppHelp(c)
+		} else {
+			data := InitMalData()
+			var mac = c.Args()[0]
+			mac = strings.Replace(mac, ":", "", -1)
+			mac = strings.Replace(mac, "-", "", -1)
+			for i := 0; i < len(data); i++ {
+				if data[i].Hex == strings.ToUpper(mac[0:6]) {
+					fmt.Println(data[i].OrgName)
+				}
+			}
+		}
+	}
+	cli.AppHelpTemplate = `NAME:
+   {{.Name}} - {{.Usage}}
+
+USAGE:
+   {{.Name}} <Address> [options]
+
+VERSION:
+   {{.Version}}{{if or .Author .Email}}
+
+AUTHOR:{{if .Author}}
+  {{.Author}}{{if .Email}} - <{{.Email}}>{{end}}{{else}}
+  {{.Email}}{{end}}{{end}}
+
+OPTIONS:
+   {{range .Flags}}{{.}}
+   {{end}}
+`
+	app.Run(os.Args)
+}
